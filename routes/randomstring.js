68bc20c6e5e295b20201019ad2f375eb81dcf992
xxRohitxx <rohitrs319791@gmail.com>
2023-01-27 14:47:00 +0530

    adding string generation

--- routes/randomstring.js
+++ routes/randomstring.js
@@ -2,8 +2,37 @@ const express=require("express")
 
 const Randomstring= express.Router()
 
+const strings ='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ~';
+
+function generateString(length) {
+    let ans = '';
+    
+    for ( let i = 0; i < length; i++ ) {
+        ans += strings.charAt(Math.floor(Math.random() * strings.length));
+    }
+
+    return ans;
+
+}
+
+
 Randomstring.get("/",(req,res)=>{
 
+
+var generatedString;
+
+if(req.query.diff=="medium"){
+    generatedString=generateString(8)
+}else if(req.query.diff=="low"){
+    generatedString=generateString(6)
+}else if(req.query.diff=="high"){
+    generatedString=generateString(12)
+}else{
+    generatedString=generateString(7)
+}
+
+
+res.send({"randomString":generatedString})
 })
 
 
